fc51718704315637eeb591ccd31dd7169cdd624f
Julija Lee <lee.julija@gmail.com>
2018-02-14 22:44:00 +0000

    adding new level0

--- level_0/vote1024.py
+++ level_0/vote1024.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 import requests
-url = 'http://158.69.76.135/level0.php?id=127&holdthedoor=Submit'
-for i in range(10):
-    requests.post(url)
+url = 'http://158.69.76.135/level0.php'
+data = {
+    'id': '127',
+    'holdthedoor': 'Submit'
+}
+for i in range(1024):
+    requests.post(url, data=data)
